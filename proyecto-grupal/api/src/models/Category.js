4084cdcf11df62ddb1842b6630a23721703239ed
xaviersieben <xaviersieben@gmail.com>
2022-11-29 22:01:06 -0300

    arreglado problema db

--- proyecto-grupal/api/src/models/Category.js
+++ proyecto-grupal/api/src/models/Category.js
@@ -3,7 +3,7 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('category', {
         id: {
-            type: DataTypes.STRING,
+            type: DataTypes.INTEGER,
             allowNull: false,
             primaryKey: true,
         },
--- proyecto-grupal/api/src/routes/ProductRoute.js
+++ proyecto-grupal/api/src/routes/ProductRoute.js
@@ -5,4 +5,6 @@ const router = Router();
 
 
 
+
+
 module.exports = router;
