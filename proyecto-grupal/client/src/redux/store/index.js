da4827ccaf91dd9e0d8028c3a9e2a3769b135dc1
William Sanabria <william_sanabria@yahoo.es>
2022-12-01 21:45:59 -0500

    william-home-pagination

--- proyecto-grupal/client/src/redux/store/index.js
+++ proyecto-grupal/client/src/redux/store/index.js
@@ -1,15 +1,32 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import productsReducer from '../reducer/productsReducer';
+// import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import thunk from 'redux-thunk';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+// import productsReducer from '../reducer/productsReducer';
 
-const reducers = combineReducers({
-	productsReducer
-});
+// const reducers = combineReducers({
+// 	productsReducer
+// });
+
+// const store = createStore(
+// 	reducers,
+// 	composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
+
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from '../reducer/productsReducer';
+import thunk from 'redux-thunk';
+
+const composeEnhancers =
+   (typeof window !== 'undefined' &&
+      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+   compose;
 
 const store = createStore(
-	reducers,
-	composeWithDevTools(applyMiddleware(thunk))
+   rootReducer,
+   composeEnhancers(applyMiddleware(thunk)),
 );
 
 export default store;
