131bd54a964e0fb0f9502f1a1da54c2556f00b6c
GonzaloIvanVallone <GonzaloValloneIvan@gmail.com>
2022-12-04 23:00:29 -0300

    fin ruta/controller User

--- proyecto-grupal/api/src/Controllers/userController.js
+++ proyecto-grupal/api/src/Controllers/userController.js
@@ -1,13 +1,28 @@
 const userServices = require('../Services/userService.js');
 const { User } = require('../db.js');
+const bcrypt = require("bcrypt");
+
+const getAllUsers = async (_req, res, next) => {
+    try{
+        const users = await User.findAll();
+        if(users.length){
+            res.status(200).json(users);
+        }else{
+            res.status(400).json({ msg: "Error, no users found"});
+        }
+    }catch(error){
+        next(error)
+    }
+}
 
 const createNewUser = async (req, res, next) => {
     const { body } = req;
+    const salt = await bcrypt.genSalt(10);
     try{
         const myUser = {
             name: body.name,
             surname: body.surname,
-            password: body.password,
+            password: await bcrypt.hash(body.password, salt),
             email: body.email,
             adress: body.adress,
         };
@@ -26,4 +41,40 @@ const createNewUser = async (req, res, next) => {
         next(error);
     }
 }
-module.exports = {createNewUser}
+
+const deleteUser = async (req, res, next) =>{
+    try{
+        const { email } = req.body;
+        const user = await User.update(
+            {active: false},
+            {where: {email: email}}
+        );
+        if(user[0]===1){
+            res.status(200).json({msg: "User found and deleted"})
+        }else{
+            res.status(400).json({msg: "User not found in the DB"});
+        }
+    }catch(error){
+        next(error)
+    }
+}
+
+const promoteUser = async(req, res, next) =>{
+    try{
+        const { id } = req.params;
+        const user = await User.update(
+            {type: 'admin'},
+            {where: {id: id}}
+        );
+        console.log(user)
+        if(user[0]===1){
+            res.status(200).json({msg: "User found and promoted"})
+        }else{
+            res.status(400).json({msg: "User not found in the DB"});
+        }
+    }catch(error){
+        next(error);
+    }
+}
+
+module.exports = {createNewUser, deleteUser, getAllUsers, promoteUser}
--- proyecto-grupal/api/src/routes/UserRoute.js
+++ proyecto-grupal/api/src/routes/UserRoute.js
@@ -1,9 +1,13 @@
 const { Router } = require("express");
 const router = Router();
-const { createNewUser } = require('../Controllers/userController');
+const { createNewUser, deleteUser, getAllUsers, promoteUser } = require('../Controllers/userController');
 
 //localhost:3001/user
 
 router.post("/", createNewUser);
+router.put("/", deleteUser);
+router.put("/:id", promoteUser);
+router.get("/", getAllUsers);
+
 
 module.exports = router;
