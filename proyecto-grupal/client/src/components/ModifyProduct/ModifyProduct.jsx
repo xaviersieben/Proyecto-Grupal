18b7e621d570653c0e496c3f398be9437252436a
DevilishSnake <joanleonelcortes@hotmail.com>
2022-12-06 00:14:24 -0300

    Creado el componente para modificar productos, cambiado el botón del detalle de productos para que lleve al componente de modificación de producto. Falta que funcione la modificación, pendiente de ser arreglado

---
+++ proyecto-grupal/client/src/components/ModifyProduct/ModifyProduct.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link, useHistory, useParams } from 'react-router-dom';
+import { getProductsDetails, postProduct } from '../../redux/actions/productsActions';
+import { getCategories } from '../../redux/actions/productsActions';
+import { useDispatch, useSelector } from 'react-redux';
+
+import styles from './ModifyProduct.module.css'
+
+import { Button } from '@mui/material';
+import ModifyForm from '../ModifyForm/ModifyForm.jsx';
+
+export default function ModifyProduct() {
+    const { id } = useParams();
+    console.log(id);
+    const dispatch = useDispatch();
+    const productDetails = useSelector((state) => state.detail);
+    const categories = useSelector((state) => state.categories);
+    const history = useHistory();
+
+
+
+    
+    
+
+    useEffect(() => {
+        dispatch(getCategories());
+        dispatch(getProductsDetails(id));
+        console.log(productDetails);
+    }, [id, dispatch]);
+
+    
+
+    return (
+        <div className={styles.fullDiv}>
+            <Link className={styles.volverAtras} to='/home'>Volver atrás...</Link>
+            { productDetails && <ModifyForm id={id} productDetails={productDetails} />}
+        </div>
+    )
+}
