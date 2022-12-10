cb843038581ad4eb72ab223f542e5b26280fc0e6
Carlos R <rinaldic@gmail.com>
2022-12-10 18:47:09 -0300

    mercadopagocommit

--- proyecto-grupal/api/src/routes/index.js
+++ proyecto-grupal/api/src/routes/index.js
@@ -5,7 +5,7 @@ const Category = require('./CategoryRoute.js');
 const Order = require('./OrderRoute');
 
 const User = require('./UserRoute.js');
-
+const createPreference = require("./checkout");
 
 
 const router = Router();
@@ -13,11 +13,9 @@ const router = Router();
 // Configurar los routers
 router.use('/product', Product);
 router.use('/category', Category);
-
 router.use('/orders', Order);
-
 router.use('/user', User);
-
+router.use('/checkout',createPreference);
 
 
 
